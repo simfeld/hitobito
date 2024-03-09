27d1f54629ee1efd4fee1a17e70392058140ec9f
Caspar <caspar.brenneisen@bitpilots.ch>
2024-03-09 15:22:33 +0100

    Update model

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,4 +1,7 @@
-class Webhook < ApplicationRecord
+class Webhook < ActiveRecord::Base
+  belongs_to :layer, class_name: 'Group', foreign_key: :layer_group_id
+
   validates :target_url, presence: true
   validates :webhook_type, presence: true
+  validates :group, presence: true
 end
